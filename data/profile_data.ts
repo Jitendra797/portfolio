0f1c668c09660086390ecb7fe2e8da0b1f3d0fee
Maddi Jitendra <116104814+Jitendra797@users.noreply.github.com>
2025-04-08 17:59:16 +0530

    refactor: add profile data including education, experiences, skills, and certificates

---
+++ data/profile_data.ts
@@ -0,0 +1,103 @@
+const educationData = [
+  {
+    school: "Raghu Engineering College, Visakhapatnam",
+    period: "2021-2025",
+    degree: "Computer Science Engineering BTech",
+    grade: "CGPA: 9.2",
+  },
+  {
+    school: "Sri Chaitanya Boys Jr. College, Nidamanuru",
+    period: "2019-2021",
+    degree: "MPC Intermediate",
+    grade: "Percentage: 96%",
+  },
+  {
+    school: "Vidya Bharathi EM School, Nagayalanka",
+    period: "2018-2019",
+    degree: "SSC (10th Standard)",
+    grade: "GPA: 10.0",
+  },
+];
+
+const experiences = [
+  {
+    date: "February 2025 - Present",
+    title: "SDE Intern",
+    company: "Aegion Dynamic Solutions",
+    description:
+      "Building web applications using NextJs, Supabase, TailwindCSS",
+  },
+  {
+    date: "July 2024 - March 2025",
+    title: "SDE Intern",
+    company: "Facttwin",
+    description:
+      "Developing web applications using Angular, NestJS, and Django frameworks. Successfully designed and implemented Todo App and Spotify Clone.",
+  },
+];
+
+const skillCategories = [
+  {
+    title: "Frontend",
+    skills: [
+      "React",
+      "Next.js",
+      "Angular",
+      "TypeScript",
+      "JavaScript",
+      "HTML5",
+      "CSS3",
+      "Tailwind CSS",
+    ],
+  },
+  {
+    title: "Backend",
+    skills: ["Node.js", "Nest.js", "Django", "GraphQL", "RESTful APIs"],
+  },
+  {
+    title: "Databases",
+    skills: ["MongoDB", "MySQL", "Firebase", "supabase", "PostgreSQL"],
+  },
+  {
+    title: "Mobile",
+    skills: ["Flutter", "Android (Kotlin)"],
+  },
+  {
+    title: "Tools & Methodologies",
+    skills: ["VsCode", "Android Studio", "Git", "GitHub", "Figma"],
+  },
+  {
+    title: "Programming Languages",
+    skills: ["C", "C++", "Java", "Python", "JavaScript", "Dart"],
+  },
+];
+
+const certificates = [
+  {
+    title: "Introduction To Programming In C",
+    issuer: "NPTEL",
+    date: "2023",
+  },
+  {
+    title: "Design and Analysis of Algorithms",
+    issuer: "NPTEL",
+    date: "2023",
+  },
+  {
+    title: "Data Base Management System",
+    issuer: "NPTEL",
+    date: "2024",
+  },
+  {
+    title: "Google Cloud Computing Foundations",
+    issuer: "NPTEL",
+    date: "2024",
+  },
+  {
+    title: "Angular",
+    issuer: "Sololearn",
+    date: "2024",
+  },
+];
+
+export { educationData, experiences, skillCategories, certificates };
